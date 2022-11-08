618904b7ddd066c27e6833b7fda842f41f85ba33
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-11-08 13:49:14 +0800

    react init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Home />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <section></section>
     </div>
   );
 }
