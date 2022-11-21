94a191985b312004f56219694a108786363ed703
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-11-21 11:29:17 +0800

    making the navigation view

---
+++ frontend/src/components/Navbarcomponent.js
@@ -0,0 +1,11 @@
+import "../css/Nav.css";
+
+const Navbarcomponent = () => {
+  return (
+    <div className="container-nav">
+      <h3>SUICARA</h3>
+    </div>
+  );
+};
+
+export default Navbarcomponent;
