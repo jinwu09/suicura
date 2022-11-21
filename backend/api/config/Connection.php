139d87e42d7a7bd4c183faf2d1dc978a2c2fafcb
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-11-21 11:28:08 +0800

    backend initial and setup for connection

--- backend/api/config/Connection.php
+++ backend/api/config/Connection.php
@@ -9,20 +9,22 @@ date_default_timezone_set("Asia/Manila");
 set_time_limit(1000);
 
 define("SERVER", "localhost");
-define("DBASE", "sample_elective");
+define("DBASE", "suicara");
 define("USER", "root");
 define("PASSWORD", "");
-define("SECRET_KEY", "aaronpogi");
+define("SECRET_KEY", "cielo");
 
-class Connection{
-    protected $con_string = "mysql:host=".SERVER.";dbname=".DBASE."; charset=utf8mb4";
+class Connection
+{
+    protected $con_string = "mysql:host=" . SERVER . ";dbname=" . DBASE . "; charset=utf8mb4";
     protected $options = [
         \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
         \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
         \PDO::ATTR_EMULATE_PREPARES => false
     ];
 
-    public function connect(){
+    public function connect()
+    {
         return new \PDO($this->con_string, USER, PASSWORD, $this->options);
     }
 }
