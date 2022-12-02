cefe77cda29182f013f2fac976c321187bc8d7b0
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-12-02 23:37:46 +0800

    update routes

--- backend/api/index.php
+++ backend/api/index.php
@@ -3,17 +3,18 @@ require_once "./config/Connection.php";
 require_once "./module/Get.php";
 require_once "./module/Post.php";
 require_once "./module/Global.php";
-require_once "./module/Auth.php";
 require_once "./module/Procedural.php";
 
+require_once "./module/data/User.php";
+require_once "./module/data/Todolist.php";
 
 $db = new Connection();
 $pdo = $db->connect();
 
 $get = new Get($pdo);
 $post = new Post($pdo);
-$auth = new Auth($pdo);
-
+$user = new User($pdo);
+$todolist = new Todolist($pdo);
 
 if (isset($_REQUEST['request'])) {
     $req = explode('/', rtrim($_REQUEST['request'], '/'));
@@ -25,6 +26,9 @@ if (isset($_REQUEST['request'])) {
 switch ($_SERVER['REQUEST_METHOD']) {
     case 'GET':
         switch ($req[0]) {
+            case 'test':
+                echo 'test';
+                break;
             case 'user':
                 if (count($req) > 1) {
                     echo json_encode($get->get_user($req[1]));
@@ -42,12 +46,32 @@ switch ($_SERVER['REQUEST_METHOD']) {
         break;
 
     case 'POST':
-        $data = receiveData(file_get_contents("php://input"));
+        $data = json_decode(file_get_contents("php://input"));
+        // $data = receiveData(file_get_contents("php://input"));
         switch ($req[0]) {
-            case 'login':
-                echo json_encode($auth->login($data));
+            case 'user':
+                switch ($req[1]) {
+                    case 'auth':
+                        echo json_encode($user->auth($data));
+                        break;
+                    default:
+                        http_response_code(403);
+                        break;
+                }
                 break;
-
+                // case 'login':
+                //     echo json_encode($user->login($data));
+                //     break;
+            case 'todolist':
+                switch ($req[1]) {
+                    case 'user':
+                        echo json_encode($todolist->todolist($data));
+                        break;
+                    default:
+                        http_response_code(403);
+                        break;
+                }
+                break;
             case 'decrypt':
                 print_r($data);
                 break;
@@ -57,7 +81,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
                 break;
 
             case 'adduser':
-                echo json_encode($auth->add_users($data));
+                echo json_encode($user->add_users($data));
                 break;
 
             case 'editstudent':
@@ -68,7 +92,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
                 echo json_encode($post->delete_student($req[1]));
                 break;
             case 'test':
-                // echo $auth->encrypt_password("Aa123456789");
+                // echo $user->encrypt_password("Aa123456789");
                 // echo $str;
                 break;
 
