7acc1bee875791f5c1e64504ef871d83a3825146
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-12-02 23:36:55 +0800

    few tweaks and adding token check

--- backend/api/module/Global.php
+++ backend/api/module/Global.php
@@ -1,59 +1,72 @@
 <?php
 
-class GlobalMethods{
+class GlobalMethods
+{
     protected $pdo;
     public function __construct(\PDO $pdo)
     {
         $this->pdo = $pdo;
     }
 
-    public function exec_query($sql){
+    public function exec_query($sql)
+    {
         $data = array();
         $errmsg = "";
         $code = 0;
 
-        try{
-            if ($result = $this->pdo->query($sql)->fetchAll()){
-                foreach ($result as $record){
+        try {
+            if ($result = $this->pdo->query($sql)->fetchAll()) {
+                foreach ($result as $record) {
                     array_push($data, $record);
                 }
                 $result = null;
                 $code = 200;
-                return array("code"=> $code, "data" => $data);
-            }
-            else{
+                return array("code" => $code, "data" => $data);
+            } else {
                 $errmsg = "No data found";
                 $code = 404;
             }
+        } catch (\PDOException $e) {
+            $errmsg = $e->getMessage();
+            $code = 403;
         }
-        catch (\PDOException $e){
-           $errmsg = $e->getMessage();
-           $code = 403;
+        return array("code" => $code, "errmsg" => $errmsg);
+    }
+
+    public function insert($table, $data)
+    {
+    }
+
+
+    public function update($table, $data, $condition_string)
+    {
+    }
+
+    public function tokencheck($user_id, $token)
+    {
+        if ($token != null) {
+            $sql = "SELECT user_id, user_token from users where user_id = ?";
+            $stmt = $this->pdo->prepare(($sql));
+            $stmt->execute([$user_id]);
+            if ($stmt->rowCount() > 0) {
+                $res = $stmt->fetchAll()[0];
+                if ($res['user_token'] === $token) {
+                    return true;
+                }
+            }
         }
-        return array("code"=> $code, "errmsg"=>$errmsg);
+        return false;
     }
 
-    public function insert($table, $data){
-
-    }
-
-
-    public function update($table, $data, $condition_string){
-        
-    }
-
-
-    public function response_payload($payload, $remarks, $message, $code){
-        $status = array("remarks"=>$remarks, "message"=> $message);
+    public function res_payload($payload, $remarks, $message, $code)
+    {
+        $status = array("remarks" => $remarks, "message" => $message);
         http_response_code($code);
         return array(
-            "status"=>$status,
-            "payload"=>$payload,
+            "status" => $status,
+            "payload" => $payload,
             "timestamp" => date_create(),
-            "prepared_by" => "Loudel M. Manaloto"
+            "prepared_by" => "Albert John B. Santos"
         );
     }
 }
-
-
-?>
