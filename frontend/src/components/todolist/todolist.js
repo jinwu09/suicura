ebf94f4a2f67b0113a40f8cb6f3e8fc78a801c70
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-12-02 13:03:11 +0800

    initial design

--- frontend/src/components/todolist/todolist.js
+++ frontend/src/components/todolist/todolist.js
@@ -9,7 +9,7 @@ const Todolist = () => {
           <div className="bg-Accent d-flex text-center px-5px justify-content-center">
             <h4>To-do List</h4>
           </div>
-          <div className="p-5px  d-flex flex-column gap-2">
+          <div className="p-5px  d-flex flex-column gap-2 ">
             <TodoButton idnum="1" todonam="sample" />
             <TodoButton idnum="2" todonam="sample2" />
           </div>
