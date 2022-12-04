7c41d7316d06a2e82c9af320157bc54b9c5df78a
JomarCCS30 <202011135@gordoncollege.edu.ph>
2022-12-04 11:09:32 +0800

    login tweeks

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,46 +1,52 @@
-import React from "react";
-import "../styles/login.css";
+import React from 'react'
+import '../styles/login.css'
 
 class Login extends React.Component {
   render() {
     return (
-      <div class="body">
-        <div class="login-box">
+      <div class='body'>
+        <div class='login-box'>
           <h1>SUICURA</h1>
-          <form action="" method="post">
-            <div class="container">
-              <label for="uname">
-                <b>Username</b>
-              </label>
-              <input
-                class="containerinput"
-                type="text"
-                placeholder="Enter Username"
-                name="uname"
-                required
-              ></input>
-              <label for="psw">
-                <b>Password</b>
-              </label>
-              <input
-                class="containerinput"
-                type="password"
-                placeholder="Enter Password"
-                name="psw"
-                required
-              ></input>
-              <button class="login" type="submit">
+          <form action='' method='post'>
+            <div class='container'>
+              <div class='usernamer'>
+                <label for='uname'>
+                  <b>Username</b>
+                </label>
+                <input
+                  class='containerinput'
+                  type='text'
+                  placeholder='Enter Username'
+                  name='uname'
+                  required
+                ></input>
+              </div>
+              <div class='password'>
+                <label for='psw'>
+                  <b>Password</b>
+                </label>
+                <input
+                  class='containerinput'
+                  type='password'
+                  placeholder='Enter Password'
+                  name='psw'
+                  required
+                ></input>
+              </div>
+            </div>
+            <div class='buttons'>
+              <button class='login' type='submit'>
                 Login
+              </button>
+              <button class='register' type='submit'>
+                Register
               </button>
             </div>
           </form>
-          <button class="register" type="submit">
-            Register
-          </button>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default Login;
+export default Login
