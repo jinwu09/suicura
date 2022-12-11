a895643baa016819e7e7ed4e89892fee99e1379e
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-12-11 23:18:21 +0800

    Revert "ChangesDone"
    This reverts commit 6e7c0fd7ed330d4191e7634a724c9ecd9cce993b.

--- frontend/src/components/CenterContent/CenterContent.js
+++ frontend/src/components/CenterContent/CenterContent.js
@@ -1,83 +1,16 @@
 import { useHomeContext } from "../context/useHomeContext";
 import Status from "./Status";
-import moment from "moment";
-import "../../styles/boxdescription.css";
 
 const CenterContent = () => {
-  const { isdashboard } = useHomeContext();
+  const { todonam, setToDoNam } = useHomeContext();
   return (
     <div>
       <div className="d-flex flex-column bg-content border-radius-15px w-90p h-98p m-2 p-3">
-        {isdashboard ? <Status /> : <Todoexpand />}
-      </div>
-    </div>
-  );
-};
-const Todoexpand = () => {
-  const { tododetail, settododetail, onsettask, getlist, liststatus } =
-    useHomeContext();
-  return (
-    <div class="box">
-      <div className="d-flex justify-content-between">
-        <h2 class="title">Task Name</h2>
-        <div className="d-flex gap-3 ">
-          <p class="datecreated1">Date created:</p>
-          <p class="datecreated2">
-            {moment(String([tododetail.todo_created])).format(
-              "dddd, MMMM Do YYYY"
-            )}
-          </p>
+        <Status />
+        <div class="">
+          <h1>{todonam}</h1>
         </div>
       </div>
-      <input
-        class="text-css1"
-        type="text"
-        value={tododetail.todo_name}
-        onChange={(e) => {
-          // let newArr = tododetail;
-          // newArr.todo_name = e.target.value;
-          // settododetail(newArr);
-
-          settododetail(() => {
-            let newArr = tododetail;
-            newArr.todo_name = e.target.value;
-            settododetail(newArr);
-          });
-
-          // settododetail_name(e.target.value);
-          console.log(tododetail);
-        }}
-      />
-      <h3 class="desc">Description</h3>
-      <div class="text-css2">
-        <textarea
-          type="text"
-          value={tododetail.todo_description}
-          className="w-maxp h-75"
-          onChange={(e) => {
-            // let newArr = tododetail;
-            // newArr.todo_description = e.target.value;
-            // settododetail(newArr);
-
-            settododetail(() => {
-              let newArr = tododetail;
-              newArr.todo_description = e.target.value;
-              settododetail(newArr);
-            });
-            console.log(tododetail);
-          }}
-        />
-      </div>
-      <button
-        className="bg-Accent p-2 rounded-3"
-        onClick={() => {
-          onsettask(tododetail.todo_id);
-          getlist(liststatus);
-          getlist(liststatus);
-        }}
-      >
-        Update
-      </button>
     </div>
   );
 };
--- frontend/src/components/todolist/todolist.js
+++ frontend/src/components/todolist/todolist.js
@@ -1,75 +1,142 @@
-import TodoButton from "./todobutton";
-import { useEffect } from "react";
-import { useHomeContext } from "../context/useHomeContext";
+import TodoButton from './todobutton'
+import { Baseurl } from '../../API/Config'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 // import "./../../styles/todo.css";
 
 const Todolist = () => {
-  const {
-    isdashboard,
-    setisdashboard,
-    setInputtodo,
-    addtodo,
-    getlist,
-    value,
-    inputtodo,
-    ondelete,
-    onarchive,
-    todolist,
-  } = useHomeContext();
+  const [todolist, settodolist] = useState([
+    { todo_id: 1, todo_name: 'default' },
+    { todo_id: 1, todo_name: 'default' },
+  ])
+  const [inputtodo, setInputtodo] = useState('')
+  const [value, setValue] = useState('')
+  const getlist = (status) => {
+    const m = JSON.parse(localStorage.getItem('m'))
+    const user_id = m.user_id
+    const user_token = m.token
+    console.log(user_token)
+    if (status === 0) {
+      axios
+        .post(Baseurl + 'user/todolist/ongoing', { user_id, user_token })
+        .then((res) => {
+          console.log(res)
+          const datas = res.data.payload
+          console.log(datas)
+
+          settodolist([...datas])
+        })
+    } else if (status === 1) {
+      axios
+        .post(Baseurl + 'user/todolist/archive', { user_id, user_token })
+        .then((res) => {
+          console.log(res)
+          const data = res.data.payload
+          console.log(data)
+
+          settodolist([...data])
+        })
+        .catch((e) => {
+          console.log(e)
+        })
+    }
+  }
+
+  const ondelete = (id) => {
+    const m = JSON.parse(localStorage.getItem('m'))
+    const user_id = m.user_id
+    const user_token = m.token
+    axios
+      .post(Baseurl + 'user/todolist/delete', {
+        user_id,
+        user_token,
+        todo_id: id,
+      })
+      .then((res) => {
+        // console.log(res);
+        settodolist(todolist.filter((todo) => todo.todo_id !== id))
+      })
+  }
+  const onarchive = (id) => {
+    const m = JSON.parse(localStorage.getItem('m'))
+    const user_id = m.user_id
+    const user_token = m.token
+    axios
+      .post(Baseurl + 'user/todolist/setarchive', {
+        user_id,
+        user_token,
+        todo_id: id,
+      })
+      .then((res) => {
+        // console.log(res);
+        settodolist(todolist.filter((todo) => todo.todo_id !== id))
+      })
+  }
+  const addtodo = (todo_name, todo_description, todo_status) => {
+    const m = JSON.parse(localStorage.getItem('m'))
+    const user_id = m.user_id
+    const user_token = m.token
+
+    axios
+      .post(Baseurl + 'user/todolist/create', {
+        user_id,
+        user_token,
+        todo_name,
+        todo_description,
+        todo_status,
+      })
+      .then((res) => {
+        console.log(res)
+        getlist(0)
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  }
 
   useEffect(() => {
-    console.log("tests");
-    getlist(0);
-  }, [value]);
+    console.log('tests')
+    getlist(0)
+  }, [value])
 
   return (
-    <div className="h-maxp bg-todolist col ">
-      <div className="d-flex flex-row ">
-        <div className="d-flex flex-column  w-maxp ">
-          <div className="todo-list-header bg-Accent d-flex text-center px-5px justify-content-center">
+    <div className='h-maxp bg-todolist col '>
+      <div className='d-flex flex-row '>
+        <div className='d-flex flex-column  w-maxp '>
+          <div className='todo-list-header bg-Accent d-flex text-center px-5px justify-content-center'>
             <h4>To-do List</h4>
           </div>
-          <button
-            className={
-              "  m-1 rounded-1 transition-all  " +
-              (isdashboard ? "bg-active" : "bg-Accent")
-            }
-            onClick={() => {
-              setisdashboard(true);
-            }}
-          >
-            Dashboard
-          </button>
-          <div className="todo-input d-flex flex-row ">
+
+          <div className='todo-input d-flex flex-row '>
             <input
-              type="text"
-              placeholder="Add todo"
+              type='text'
+              placeholder='Add todo'
               onChange={(e) => {
-                setInputtodo(e.target.value);
+                setInputtodo(e.target.value)
               }}
             />
             <button
-              className="bg-accentcontent m-2 p-2"
-              onClick={() => addtodo(inputtodo, "", 0)}
+              className='bg-accentcontent m-2 p-2'
+              onClick={() => addtodo(inputtodo, '', 0)}
             >
               Add
             </button>
           </div>
-          <div className="status-button">
+          <div className='status-button'>
             <button
-              className="bg-accentcontent  p-2"
+              className='bg-accentcontent  p-2'
               onClick={() => getlist(0)}
             >
               Ongoing
             </button>
             <button
-              className="bg-accentcontent  p-2"
+              className='bg-accentcontent  p-2'
               onClick={() => getlist(1)}
             >
               Archived
             </button>
           </div>
-          <div className="p-5px  d-flex flex-column gap-2 ">
+          <div className='p-5px  d-flex flex-column gap-2 '>
             {todolist.map((obj, index) => {
               return (
                 <TodoButton
@@ -80,12 +147,12 @@ const Todolist = () => {
                   onClickDelete={ondelete}
                   onClickArchive={onarchive}
                 />
-              );
+              )
             })}
           </div>
         </div>
       </div>
     </div>
-  );
-};
-export default Todolist;
+  )
+}
+export default Todolist
