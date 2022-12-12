fef62f34fbd8965e2bc03a7812fb01fa822be814
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-12-12 13:11:37 +0800

    revision pomodoro

--- frontend/src/components/pomodoro/pomodoro.js
+++ frontend/src/components/pomodoro/pomodoro.js
@@ -1,9 +1,12 @@
 // import "./../../styles/pomodoro.css";
-import { useTimer } from 'react-timer-hook'
+import { useState } from "react";
+import { useTimer } from "react-timer-hook";
 
 const Pomodoro = () => {
-  const time = new Date()
-  const expiryTimestamp = time.setMinutes(time.getMinutes() + 25)
+  const [inputtimesec, setinputtimesec] = useState(0);
+  const [inputtimemin, setinputtimemin] = useState(0);
+  const time = new Date();
+  const expiryTimestamp = time.setMinutes(time.getMinutes() + 25);
   const {
     seconds,
     minutes,
@@ -17,63 +20,116 @@ const Pomodoro = () => {
   } = useTimer({
     expiryTimestamp,
     autoStart: false,
-    onExpire: () => console.warn('onExpire called'),
-  })
+    onExpire: () => console.warn("onExpire called"),
+  });
+
+  const formatTime = (time) => {
+    return String(time).padStart(2, "0");
+  };
 
   return (
-    <div className=' bg-pomodoro'>
-      <div className='pomodoro'>
-        <div className='d-flex gap-2 flex-column'>
-          <button className='text-pomodoro text-shadow-sm text-2rem'>
+    <div className=" bg-pomodoro">
+      <div className="pomodoro">
+        <div className="d-flex gap-2 flex-column">
+          <button className="text-pomodoro text-shadow-sm text-2rem">
             Timer
           </button>
         </div>
-        <div className='timer-content'>
+        <form
+          className="d-flex flex-row "
+          onSubmit={(e) => {
+            e.preventDefault();
+            const time = new Date();
+            time.setMinutes(time.getMinutes() + inputtimemin * 1);
+            time.setSeconds(time.getSeconds() + inputtimesec * 1);
+            restart(time);
+          }}
+        >
+          <input
+            className="w-4rem"
+            type="number"
+            min={0}
+            // max={60}
+            value={inputtimemin}
+            onChange={(e) => {
+              setinputtimemin(e.target.value);
+            }}
+            placeholder="Min"
+          />
+          <input
+            className="w-4rem"
+            type="number"
+            value={inputtimesec}
+            min={0}
+            max={60}
+            onChange={(e) => {
+              setinputtimesec(e.target.value);
+            }}
+            placeholder="Sec"
+          />
+          <button className="p-1 mx-2 bg-active rounded-1 ">set</button>
           <button
-            className='text-pomodoro time-duration text-shadow-sm text-1rem '
+            className="p-1 mx-2 bg-active rounded-1"
             onClick={() => {
-              const time = new Date()
-              time.setMinutes(time.getMinutes() + 15)
-              restart(time)
+              setinputtimemin("");
+              setinputtimesec("");
+            }}
+          >
+            Reset
+          </button>
+        </form>
+        <div className="timer-content">
+          <button
+            className="text-pomodoro time-duration text-shadow-sm text-1rem "
+            onClick={() => {
+              const time = new Date();
+              time.setMinutes(time.getMinutes() + 15);
+              restart(time);
             }}
           >
             Short Duration
           </button>
           <button
-            className='text-pomodoro time-duration text-shadow-sm text-1rem'
+            className="text-pomodoro time-duration text-shadow-sm text-1rem"
             onClick={() => {
-              const time = new Date()
-              time.setMinutes(time.getMinutes() + 25)
-              restart(time)
+              const time = new Date();
+              time.setMinutes(time.getMinutes() + 25);
+              restart(time);
             }}
           >
             Long Duration
           </button>
           <button
             className={
-              'text-pomodoro pomodoro-time text-shadow-sm text-4rem rounded-4 transition-all ' +
-              (isRunning ? 'color-active' : 'bg-accent')
+              "text-pomodoro pomodoro-time text-shadow-sm text-4rem rounded-4 transition-all " +
+              (isRunning ? "color-active" : "bg-accent")
             }
             onClick={() => {
               if (isRunning === true) {
-                pause()
+                pause();
               } else {
-                start()
+                resume();
               }
             }}
           >
-            {' '}
-            {minutes}:{seconds}
+            {" "}
+            {formatTime(minutes + hours * 60)}:{formatTime(seconds)}
           </button>
         </div>
         <button
-          className='start text-pomodoro text-shadow-sm '
-          onClick={() => start()}
+          className="start text-pomodoro text-shadow-sm transition-all"
+          onClick={() => {
+            if (isRunning === true) {
+              pause();
+            } else {
+              resume();
+            }
+          }}
         >
-          START
+          {isRunning ? "PAUSE" : "START"}
         </button>
       </div>
     </div>
-  )
-}
-export default Pomodoro
+  );
+};
+export default Pomodoro;
--- readme.md
+++ readme.md
@@ -1,22 +1,22 @@
 trello todo list
 https://trello.com/b/8xkmrYZE/err-code
-- frontend
-  - login view (done)
-    - design (done)
-    - functionality
-  - dashboard View 
-  - Pomodoro Component
-    - design (done)
-    - functionality
+[] frontend
+  [x] login view (done)
+  [x] design (done)
+  [x] functionality
+  [x] dashboard View 
+  [x] Pomodoro Component
+    [x] design (done)
+    [x] functionality
+  [] encryp
   - File system component
     - design (done)
     - functionality
-  -  
   - Encryption
   - Decryption
 - Backend 
   - Encryption 
   - Decryption
-  - Login/auth endpoints (done)
-    - token auth endpoint 
-  - Get all todo list endpoint
+  [x] Login/auth endpoints (done)
+    [x] token auth endpoint 
+  [x] Get all todo list endpoint
